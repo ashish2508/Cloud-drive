7ea67d5d16967f3e14c4831773e630f373c71387
ashish2508 <jha250805@gmail.com>
2025-05-12 19:59:10 +0530

    page work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-import {db} from "~/server/db"
-import {files_table as filesSchema, folders_table as foldersSchema} from "~/server/db/schema";
+import { db } from "~/server/db";
+import {
+  files_table as filesSchema,
+  folders_table as foldersSchema,
+} from "~/server/db/schema";
 import DriveContents from "./sandbox/drive-content";
+
 export default async function GoogleDriveClone() {
- const files = await db.select().from(filesSchema)
-  const folders = await db.select().from(foldersSchema)
-  return <DriveContents files={files} folders={folders} />
+  const files = await db.select().from(filesSchema);
+  const folders = await db.select().from(foldersSchema);
+  return <DriveContents files={files} folders={folders} />;
 }
