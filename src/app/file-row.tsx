cc8fe79de7bbab282afd342dde8d7b3bdec20669
ashish2508 <jha250805@gmail.com>
2025-05-16 03:24:10 +0530

    sandbox

--- src/app/file-row.tsx
+++ src/app/file-row.tsx
@@ -1,13 +1,10 @@
-import { FileIcon, Folder as FolderIcon } from "lucide-react";
-import type {
-  files_table,
-  folders_table,
-} from "~/server/db/schema";
+import { Folder as FolderIcon, FileIcon, Trash2Icon } from "lucide-react";
 import Link from "next/link";
+import { Button } from "~/components/ui/button";
+import { deleteFile } from "~/server/actions";
+import type { folders_table, files_table } from "~/server/db/schema";
 
-export function FileRow(props: {
-  file: typeof files_table.$inferSelect;
-}) {
+export function FileRow(props: { file: typeof files_table.$inferSelect }) {
   const { file } = props;
   return (
     <li
@@ -25,8 +22,17 @@ export function FileRow(props: {
             {file.name}
           </a>
         </div>
-        <div className="col-span-3 text-gray-400">{"file"}</div>
+        <div className="col-span-2 text-gray-400">{"file"}</div>
         <div className="col-span-3 text-gray-400">{file.size}</div>
+        <div className="col-span-1 text-gray-400">
+          <Button
+            variant="ghost"
+            onClick={() => deleteFile(file.id)}
+            aria-label="Delete file"
+          >
+            <Trash2Icon size={20} />
+          </Button>
+        </div>
       </div>
     </li>
   );
@@ -38,12 +44,11 @@ export function FolderRow(props: {
   const { folder } = props;
   return (
     <li
-      key={folder.id} // This will now work
+      key={folder.id}
       className="hover:bg-gray-750 border-b border-gray-700 px-6 py-4"
     >
       <div className="grid grid-cols-12 items-center gap-4">
         <div className="col-span-6 flex items-center">
-
           <Link
             href={`/f/${folder.id}`}
             className="flex items-center text-gray-100 hover:text-blue-400"
@@ -51,7 +56,6 @@ export function FolderRow(props: {
             <FolderIcon className="mr-3" size={20} />
             {folder.name}
           </Link>
-
         </div>
         <div className="col-span-3 text-gray-400"></div>
         <div className="col-span-3 text-gray-400"></div>
